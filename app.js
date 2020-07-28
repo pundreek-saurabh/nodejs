2d2e0aaaed5f829ba4f38064d102b1f2b3fb82f1
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-07-28 09:21:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
+
+/*=========================================================
+                    BODY-PARSER
+===========================================================
+*/
+
+const body =(req, res, next) =>{
+  console.log('Body-parser middleware');
+  next();
+}; 
+
+
+/*=========================================================
+                            CORS
+===========================================================
+*/
+
+//To enable all cors request.
+const element =(req, res, next) =>{
+  console.log('cors middleware');
+  next();
+};
+
+
+/*=========================================================
+                            MORGAN
+===========================================================
+*/
+
+const morgan =(req, res, next) =>{
+  console.log('Morgan middleware');
+  next();
+};
