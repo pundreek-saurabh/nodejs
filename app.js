3c4ed949b7c028372785baed80e0db71d9654fd7
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-07-30 20:27:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,54 +15,22 @@ app.listen(PORT, () => {
                     BODY-PARSER
 ===========================================================
 */
-/* Adding a generic json middleware */
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
- 
-app.use((req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
 
-/*  POST /login gets urlencoded bodies */
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-app.post('/login', urlencodedParser ,(req,res)=>{
-    console.log(req.body);
-    res.send('welcome')
-});
-
-// POST /api/users gets JSON bodies
-app.post('/api/users', jsonParser, function (req, res) {    
-  })
 
 /*=========================================================
                             CORS
 ===========================================================
 */
 
-//To enable all cors request.
-app.get('/products/:id', (req, res, next) => {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-  
-  app.listen(80,  () =>{
-    console.log('CORS-enabled web server listening on port :"3000" ')
-  })
+app.use(cors());
+
 
 
 /*=========================================================
                             MORGAN
 ===========================================================
 */
-
-// create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
  
-// setup the logger
-app.use(morgan('combined', { stream: accessLogStream }))
- 
-app.get('/', function (req, res) {
-  res.send('hello, world!')
-});
+app.use(morgan('dev'));
